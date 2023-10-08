0f5f96bca9af9c365db2eb84ea5640f8718500e8
LAPTOP-KOTOV <ekotovqa@gmail.com>
2023-10-08 23:38:01 +0200

    Added authorization

---
+++ src/gRPChat/Frontend/Web/gRPChat.Web/AuthorizeAPI.cs
@@ -0,0 +1,29 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace gRPChat.Web
+{
+    public class AuthorizeAPI
+    {
+        private readonly IdentityAuthenticationStateProvider _authenticationStateProvider;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthorizeAPI(AuthenticationStateProvider authenticationStateProvider, ILocalStorageService localStorage)
+        {
+            _authenticationStateProvider = (IdentityAuthenticationStateProvider) authenticationStateProvider;
+            _localStorage = localStorage;
+        }
+
+        public async Task<bool> Login(string login, string password)
+        {
+            var token = "123456789";
+
+
+            await _localStorage.SetItemAsync("token", token);
+
+            _authenticationStateProvider.MarkUserAsAuthenticated(token);
+
+            return true;
+        }
+    }
+}
---
+++ src/gRPChat/Frontend/Web/gRPChat.Web/IdentityAuthenticationStateProvider.cs
@@ -0,0 +1,42 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+public class IdentityAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private readonly ILocalStorageService _localStorage;
+
+    public IdentityAuthenticationStateProvider(ILocalStorageService localStorage)
+    {
+        _localStorage = localStorage;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var token = await _localStorage.GetItemAsStringAsync("token");
+
+        if(!string.IsNullOrEmpty(token))
+        {
+            var authUser = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "admin"),
+            }, "jwt"));
+
+            return new AuthenticationState(authUser);
+        }
+
+        return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+    }
+
+    public void MarkUserAsAuthenticated(string token)
+    {
+        var authUser = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, "admin"),
+        }, "jwt"));
+
+        var authState = Task.FromResult(new AuthenticationState(authUser));
+
+        NotifyAuthenticationStateChanged(authState);
+    }
+}
