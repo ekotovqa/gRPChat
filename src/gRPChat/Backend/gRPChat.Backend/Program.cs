0f973380dd2dc5257606788d81d5169362ac3ebc
LAPTOP-KOTOV <ekotovqa@gmail.com>
2023-11-15 21:28:13 +0100

    Kestrel configured

--- src/gRPChat/Backend/gRPChat.Backend/Program.cs
+++ src/gRPChat/Backend/gRPChat.Backend/Program.cs
@@ -3,7 +3,9 @@ using gRPChat.Database;
 using gRPChat.Database.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -51,6 +53,24 @@ builder.Services.AddCors(options => options.AddPolicy("AllowAll", builder =>
 builder.Services.AddAuthorization();
 
 builder.Services.AddSingleton<ChatRoomManager>();
+
+builder.WebHost.UseKestrel(serverOptions =>
+{
+    var port = 5003;
+    var pfxFilePath = @"C:\Users\evgen\Documents\Certificates\localhost.cer";
+    // The password you specified when exporting the PFX file using OpenSSL.
+    // This would normally be stored in configuration or an environment variable;
+    // I've hard-coded it here just to make it easier to see what's going on.
+    var pfxPassword = "green cairo angle piano";
+
+    serverOptions.Listen(IPAddress.Any, port, listenOptions =>
+    {
+        // Enable support for HTTP1 and HTTP2 (required if you want to host gRPC endpoints)
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+        // Configure Kestrel to use a certificate from a local .PFX file for hosting HTTPS
+        listenOptions.UseHttps(pfxFilePath);
+    });
+});
 
 var app = builder.Build();
 
